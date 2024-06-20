7fa665231304f40bd181545a76a9ac3768b8fdef
Sengho825 <53143098+Sengho825@users.noreply.github.com>
2024-06-20 15:02:37 +0700

    update

--- erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
+++ erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
@@ -781,26 +781,35 @@ def get_pending_stock_count():
 def get_item_qty_from_warehouse(param):
 	import json
 	param = json.loads(param)
-	param['warehouse']
-	param['item_code']
 
 	if frappe.db.exists("Bin", {"warehouse": param['warehouse'],"item_code": param['item_code']}):
 
 		data = frappe.db.get_value("Bin",{"warehouse": param['warehouse'],"item_code": param['item_code']},['stock_uom',"item_code",'actual_qty','item_name'],as_dict=1)
 		sql = "select qty from `tabStock Reconciliation Item` where parent = '{0}' and item_code = '{1}'".format(param['name'],param['item_code'])
-
+		data['alternate_item_code'] = param['item_code']
 		old_qty = frappe.db.sql(sql,as_dict=1)
 		if old_qty:
 			data["qty"] = old_qty[0]['qty'] or 0
 		return data
+
 	else:
 		data = frappe.db.get_value("Item",param['item_code'],['stock_uom','item_name','item_code'],as_dict=1)
-		
+		if not data:
+			data = frappe.db.get_value("Item",{'alternate_item_code':param['item_code']} ,['stock_uom','item_name','item_code'],as_dict=1)
+			if  not data:
+				frappe.throw('Product Code {} Not  Exist!'.format(param['item_code']))
+				
+			if frappe.db.exists("Bin", {"warehouse": param['warehouse'],"item_code": data.item_code}):
+				data = frappe.db.get_value("Bin",{"warehouse": param['warehouse'],"item_code": data.item_code},['stock_uom',"item_code",'actual_qty','item_name'],as_dict=1)
+				sql = "select qty from `tabStock Reconciliation Item` where parent = '{0}' and item_code = '{1}'".format(param['name'],param['item_code'])
+				data['alternate_item_code'] = param['item_code']
+				old_qty = frappe.db.sql(sql,as_dict=1)
+				if old_qty:
+					data["qty"] = old_qty[0]['qty'] or 0
 		
 		if data:
-			data = {"item_code":data.item_code,"stock_uom":data.sotkc_uom,"item_name":data.item_name,"actual_qty":0,"qty":0}
+			data = {"item_code":data.item_code,"stock_uom":data.sotkc_uom,"item_name":data.item_name,"actual_qty":0,"qty":0,"alternate_item_code":param['item_code']}
 			sql = "select qty from `tabStock Reconciliation Item` where parent = '{0}' and item_code = '{1}'".format(param['name'],param['item_code'])
-		
 			old_qty = frappe.db.sql(sql,as_dict=1)
 			if old_qty:
 				data["qty"] = old_qty[0]['qty'] or 0
@@ -815,8 +824,6 @@ def get_item_qty_from_warehouse_contain_code(param):
 	data =   frappe.db.get_list("Bin",fields=['stock_uom',"item_code",'actual_qty','item_name'],filters=[["Bin","warehouse","=",param["warehouse"]],["Bin","item_code","like","%{}%".format(param["item_code"])]], page_length=20)
 
 	if data:
-
-		
 		sql = "select item_code, sum(qty) as qty from `tabStock Reconciliation Item` where parent = '{0}' and item_code in %(item_codes)s group by item_code".format(param['name'])
 
 		old_qty_data = frappe.db.sql(sql,{"item_codes":[d.item_code for d in data ]},as_dict=1)
@@ -834,9 +841,38 @@ def get_item_qty_from_warehouse_contain_code(param):
 			for p in data:
 				p["qty"] = sum([d["qty"] for d in old_qty_data if d["item_code"]==p["item_code"]])
 			return data
-		 
+		else:
+			sql = "select item_code, sum(qty) as qty from `tabStock Reconciliation Item` where parent = '{0}' and alternate_item_code in %(item_codes)s group by item_code".format(param['name'])
+
+			old_qty_data = frappe.db.sql(sql,{"item_codes":[d["item_code"] for d in data ]},as_dict=1)
+			for p in data:
+				p["qty"] = sum([d["qty"] for d in old_qty_data if d["item_code"]==p["item_code"]])
+			return data
 
 	frappe.throw('Product Code {} Not  Exist!'.format(param['item_code']))
+
+
+@frappe.whitelist()
+def save_stock_reconcil(param):
+	import json
+	param = json.loads(param)
+
+	doc = frappe.get_doc("Stock Reconciliation",param['name'])
+	for d in param['items']:
+		child_doc = [x for x in doc.items if x.item_code == d['item_code']]
+		if child_doc:
+			child_doc[0].qty = d['qty']
+		else:
+			user  = frappe.db.get_value("User",frappe.session.user,['email','full_name'],as_dict=1)
+			doc.append("items",{
+				"item_code":d['item_code'],
+				"qty":d['qty'],
+				"warehouse":param['set_warehouse'],
+				"counted_user":frappe.session.user,
+				"counted_by":user.full_name
+
+			})
+	doc.save(ignore_permissions=True)
 
 @frappe.whitelist()
 def save_stock_reconcil(param):
