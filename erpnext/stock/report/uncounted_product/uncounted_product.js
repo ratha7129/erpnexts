b5251a249b2bde6562ddb4af884d70f91f21a338
Sengho825 <53143098+Sengho825@users.noreply.github.com>
2024-07-13 11:20:50 +0700

    update

--- erpnext/stock/report/uncounted_product/uncounted_product.js
+++ erpnext/stock/report/uncounted_product/uncounted_product.js
@@ -13,17 +13,19 @@ frappe.query_reports["Uncounted Product"] = {
 		{
 			"fieldname":"name",
 			"label": __("Stock Reconciliation"),
-			"fieldtype": "Link",
+			"fieldtype": "MultiSelectList",
 			"reqd": 1,
 			"on_change": function (query_report) {},
-			"options": "Stock Reconciliation",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Stock Reconciliation', txt);
+			},
 		},
 		{
 			fieldname: "item_group",
 			label: "Item Group",
 			fieldtype: "MultiSelectList",
 			get_data: function(txt) {
-				return frappe.db.get_link_options('Item Group', txt);
+				return frappe.db.get_link_options('Item Group', txt,{"is_group":1});
 			},
 			"on_change": function (query_report) {},
 			 
@@ -34,7 +36,11 @@ frappe.query_reports["Uncounted Product"] = {
 			fieldtype: "Select",
 			options:"Uncounted Item\nCounted Item",
 			default:"Uncounted Item",
-			"on_change": function (query_report) {},
+			on_change: function() {
+				let status = frappe.query_report.get_filter_value('status');
+				frappe.query_report.toggle_filter_display('item_group', status === 'Counted Item');
+				// frappe.query_report.refresh();
+			}
 			 
 		},
 	]
--- erpnext/stock/report/uncounted_product/uncounted_product.py
+++ erpnext/stock/report/uncounted_product/uncounted_product.py
@@ -9,6 +9,9 @@ def execute(filters=None):
 
 	if not filters:
 		filters = {}
+	if len(filters.item_group) <= 0 and filters.status != "Counted Item":
+		frappe.throw("Please select item group.")
+
 	columns = get_columns(filters)
 	stock = get_data(filters)
 
@@ -21,8 +24,9 @@ def get_columns(filters):
 			{"label":"Item", "fieldname":"item_code","fieldtype":"Link","options":"Item","align":"left","sql":"item_code","width":"250"},
 			{"label":"Item Name", "fieldname":"item_name","fieldtype":"Data","align":"left","width":"150"},
 			{"label":"Supplier", "fieldname":"supplier_name","fieldtype":"Data","align":"left","width":"150"},
-			{"label":"Min Quantity", "fieldname":"min_quantity","fieldtype":"Data","align":"left","width":"60"},
-			{"label":"Max Quantity", "fieldname":"max_quantity","fieldtype":"Data","align":"left","width":"60"},
+			{"label":"Item Group", "fieldname":"item_group","fieldtype":"Data","align":"left","width":"150"},
+			{"label":"Min Quantity", "fieldname":"min_quantity","fieldtype":"Data","align":"center","width":"60"},
+			{"label":"Max Quantity", "fieldname":"max_quantity","fieldtype":"Data","align":"center","width":"60"},
 			{"label":"BOH", "fieldname":"actual_qty","fieldtype":"Int","width":"100"},
 			{"label":"UOM", "fieldname":"stock_uom","fieldtype":"Data","width":"150"},
 			{"label":"Cost", "fieldname":"cost","fieldtype":"Currency","width":"150"},
@@ -34,24 +38,28 @@ def get_columns(filters):
 		return [
 			{"label":"Item", "fieldname":"item_code","fieldtype":"Link","options":"Item","align":"left","sql":"item_code","width":"250"},
 			{"label":"Item Name", "fieldname":"item_name","fieldtype":"Data","align":"left","width":"150"},
-			{"label":"BOH", "fieldname":"current_quantity","fieldtype":"Int","width":"100"},
-			{"label":"QTY", "fieldname":"qty","fieldtype":"Data","width":"150"},
-			{"label":"Diff", "fieldname":"quantity_difference","fieldtype":"Currency","width":"150"},
+			{"label":"BOH", "fieldname":"current_qty","fieldtype":"Int","width":"100"},
+			{"label":"QTY", "fieldname":"qty","fieldtype":"Int","width":"150"},
+			{"label":"Diff", "fieldname":"quantity_difference","fieldtype":"Int","width":"150"},
 			{"label":"Cost", "fieldname":"valuation_rate","fieldtype":"Currency","width":"150"},
 			{"label":"Diff Cost", "fieldname":"amount_difference","fieldtype":"Currency","width":"150"}
 		]
 
 
 def get_data(filters):
-	all_product_category_sql = """
-		select 
-			name 
-		from `tabItem Group`
-		where parent_item_group = %(item_group)s
-	"""
-	category = frappe.db.sql(all_product_category_sql,filters,as_dict=1)
+	
+	
 	item_sql = ""
-	if len(category) > 0:
+	if len(filters.item_group) > 0:
+		all_product_category_sql = """
+			select 
+				name 
+			from `tabItem Group`
+			where parent_item_group in %(item_group)s
+		"""
+	
+		category = frappe.db.sql(all_product_category_sql,filters,as_dict=1)
+	
 		category = [c['name'] for c in category]
 	#get Product in category
 	if filters.status == "Uncounted Item":
@@ -60,6 +68,7 @@ def get_data(filters):
 			item.item_code,
 			item.item_name,
 			item.supplier_name,
+			item.item_group,
 			item.max_quantity,
 			item.min_quantity,
 			coalesce(bin.actual_qty,0) actual_qty,
@@ -68,29 +77,14 @@ def get_data(filters):
 			item.standard_rate,
 			item.wholesale_price,
 			item.max_discount
-			from `tabItem` item 
+			from `tabStock Reconciliation Item` ri
+			right join `tabItem` item on item.name = ri.name
 			left join `tabBin` bin on item.name = bin.item_code
 			where 
 				item.item_group in %(category)s and 
-				item.name not in (select item_code from `tabStock Reconciliation Item` where parent = %(name)s) 
+				item.name not in (select item_code from `tabStock Reconciliation Item` where parent in %(name)s) 
 
-			UNION
-
-			select 
-				a.item_code,
-				a.item_name,
-				b.supplier_name,
-				b.max_quantity,
-				b.min_quantity,
-				a.current_qty,
-				b.stock_uom,
-				a.valuation_rate cost,
-				b.standard_rate,
-				b.wholesale_price,
-				b.max_discount
-			from `tabNone Change Stock Reconciliation Item` a
-			inner join `tabItem` b
-			where a.stock_reconciliation = %(name)s
+			
 		"""
 		stock_reconcill_item = frappe.db.sql(item_sql,{"category":category,"name":filters.name},as_dict=1)
 	else:
@@ -101,13 +95,28 @@ def get_data(filters):
 				current_qty,
 				qty,
 				quantity_difference,
-				valuation_rate
+				valuation_rate,
+				amount_difference
 			from `tabStock Reconciliation Item`
-			where parent = %(name)s
+			where parent in %(name)s
+
+			UNION
+
+			select 
+				a.item_code,
+				a.item_name,
+				a.current_qty,
+				a.qty,
+				0 quantity_difference,
+				b.valuation_rate,
+				0 amount_difference
+			from `tabNone Change Stock Reconciliation Item` a
+			inner join `tabItem` b
+			where a.stock_reconciliation in %(name)s
 		"""
 		stock_reconcill_item = frappe.db.sql(item_sql,{"name":filters.name},as_dict=1)
-	
-	
-	
+		
+		
+		
 	return stock_reconcill_item
 
